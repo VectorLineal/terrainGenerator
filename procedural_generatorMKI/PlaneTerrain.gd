5500e74a12a633fafe273bd388a68bd2c174c9be
VectorLineal <jnobzaa@unal.edu.co>
2021-08-03 10:15:18 -0500

    se controla ruido simplex manejando parámetros desde GDScript y se pone color de terreno

--- procedural_generatorMKI/PlaneTerrain.gd
+++ procedural_generatorMKI/PlaneTerrain.gd
@@ -1,14 +1,29 @@
 extends MeshInstance
 
-
-# Declare member variables here. Examples:
-# var a = 2
 var heightMap
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(self.get_surface_material(0).get_shader_param("map").get_type())
+	#Generación de terreno base se hace usando ruido simplex (implementación que Godot ya posee)
+	#print(self.get_surface_material(0).get_shader_param("map"))
+	var noise = OpenSimplexNoise.new()
+	noise.seed = randi()
+	noise.octaves = 4
+	noise.period = 64.0
+	noise.persistence = 0.5
+	noise.lacunarity = 2.0
+	self.heightMap = NoiseTexture.new()
+	self.heightMap.noise = noise
+	print(heightMap.height, heightMap.width)
+	self.get_surface_material(0).set_shader_param("map", heightMap)
+	self.get_surface_material(0).set_shader_param("height_scale", 0.7)
+	
+	#Paso de ajuste de terreno y simulación usando algún métodos físicos (erosión física y termal).
+	
+	#Paso de refinamiento de terreno usando técnicas de sistemas inteligentes
+	#El texturizado se hace desde el fragment shader dependiendo de clima, humedad, altura, bioma, etc.
+	
+	#Paso de pocisionamiento de assets para dar mayor detalle al terreno
 	
 
 
