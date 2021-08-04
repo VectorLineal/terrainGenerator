6ff2f8ec24be1a19543e41a1c725d2e073fdc3e8
VectorLineal <jnobzaa@unal.edu.co>
2021-08-04 11:17:30 -0500

    se agrega mapa de biomas con temperatura humedad y viento codificado en forma de imagen

--- procedural_generatorMKI/PlaneTerrain.gd
+++ procedural_generatorMKI/PlaneTerrain.gd
@@ -1,13 +1,17 @@
 extends MeshInstance
 
 var heightMap
+var biomeMap
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Generación de terreno base se hace usando ruido simplex (implementación que Godot ya posee)
 	#print(self.get_surface_material(0).get_shader_param("map"))
 	var noise = OpenSimplexNoise.new()
+	#reemplazar por semilla suministrada por el usuario
 	noise.seed = randi()
+	rng.seed = noise.seed
 	noise.octaves = 4
 	noise.period = 64.0
 	noise.persistence = 0.5
@@ -19,6 +23,20 @@ func _ready():
 	self.get_surface_material(0).set_shader_param("height_scale", 0.7)
 	
 	#Paso de ajuste de terreno y simulación usando algún métodos físicos (erosión física y termal).
+	#Se crea mapa de temperatura (R), humedad (G), vientos (BA)
+	self.biomeMap = ImageTexture.new()
+	var image = Image.new()
+	image.create(512, 512, false, Image.FORMAT_RGBAF)
+	image.lock()
+	for y in image.get_height():
+		for x in image.get_width():
+			image.set_pixel(x, y, Color(self.rng.randf(), self.rng.randf(), self.rng.randf(), self.rng.randf()))
+	
+	image.unlock()
+	
+	self.biomeMap.create_from_image(image)
+	print("biome ", biomeMap.get_data().get_height(), biomeMap.get_data().get_width(), ", ", self.biomeMap.get_data())
+	self.get_surface_material(0).set_shader_param("biome_map", biomeMap)
 	
 	#Paso de refinamiento de terreno usando técnicas de sistemas inteligentes
 	#El texturizado se hace desde el fragment shader dependiendo de clima, humedad, altura, bioma, etc.
--- procedural_generatorMKI/terraformer.shader
+++ procedural_generatorMKI/terraformer.shader
@@ -3,13 +3,14 @@ shader_type spatial;
 
 uniform float height_scale = 0.5;
 uniform sampler2D map;
+uniform sampler2D biome_map;
 uniform vec4 color;
 
 void vertex() {
 	float height = texture(map, VERTEX.xz / 2.0 + 0.5).x * height_scale;
 	//float height = fbm(VERTEX.xz * 4.0) * height_scale;
 	VERTEX.y += height * 0.5;
-  	COLOR.xyz = vec3(height) * color.xyz;
+  	COLOR.xyz = vec3(height) * texture(biome_map, VERTEX.xz / 2.0 + 0.5).xyz;
 	vec2 e = vec2(0.01, 0.0);
 	vec3 normal = normalize(vec3(texture(map, VERTEX.xz / 2.0 - e).x - texture(map, VERTEX.xz / 2.0 + e).x, 2.0 * e.x, texture(map, VERTEX.xz / 2.0 - e.yx).x - texture(map, VERTEX.xz / 2.0 + e.yx).x));
 	NORMAL = normal;
