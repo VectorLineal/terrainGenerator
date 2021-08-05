d0bfa0f134d6c5d383dfd79fdf4d18d1c43db2e8
VectorLineal <jnobzaa@unal.edu.co>
2021-08-05 11:43:16 -0500

    temperature map is generated based on height map

--- procedural_generatorMKI/PlaneTerrain.gd
+++ procedural_generatorMKI/PlaneTerrain.gd
@@ -3,6 +3,13 @@ extends MeshInstance
 var heightMap
 var biomeMap
 var rng = RandomNumberGenerator.new()
+var seaLevel = 0
+var maxTemperature = 30
+var minTemperature = 0
+
+func remap(iMin, iMax, oMin, oMax, v):
+	var t = inverse_lerp(iMin, iMax, v)
+	return lerp(oMin, oMax, t)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,15 +17,16 @@ func _ready():
 	#print(self.get_surface_material(0).get_shader_param("map"))
 	var noise = OpenSimplexNoise.new()
 	#reemplazar por semilla suministrada por el usuario
-	noise.seed = randi()
+	noise.seed = 10000#randi()
 	rng.seed = noise.seed
 	noise.octaves = 4
 	noise.period = 64.0
 	noise.persistence = 0.5
 	noise.lacunarity = 2.0
-	self.heightMap = NoiseTexture.new()
-	self.heightMap.noise = noise
-	print(heightMap.height, heightMap.width)
+	self.heightMap = ImageTexture.new()
+	var heightImage = noise.get_image(512, 512)
+	self.heightMap.create_from_image(heightImage)
+	print("biome ", heightMap.get_data().get_height(), heightMap.get_data().get_width())
 	self.get_surface_material(0).set_shader_param("map", heightMap)
 	self.get_surface_material(0).set_shader_param("height_scale", 0.7)
 	
@@ -28,15 +36,22 @@ func _ready():
 	var image = Image.new()
 	image.create(512, 512, false, Image.FORMAT_RGBAF)
 	image.lock()
+	heightImage.lock()
 	for y in image.get_height():
 		for x in image.get_width():
-			image.set_pixel(x, y, Color(self.rng.randf(), self.rng.randf(), self.rng.randf(), self.rng.randf()))
+			#image.set_pixel(x, y, Color(self.rng.randf(), self.rng.randf(), self.rng.randf(), self.rng.randf()))
+			if heightImage.get_pixel(x, y).r >= 2.0 / 3.0 or heightImage.get_pixel(x, y).r <= 8.0 / 9.0:
+				image.set_pixel(x, y, Color(1 - remap(minTemperature, maxTemperature, 0, 1, heightImage.get_pixel(x, y).r * (maxTemperature - minTemperature)), 0, 0, 1))
+			else:
+				image.set_pixel(x, y, Color(1 - self.rng.randf_range(2.0 / 3.0,8.0 / 9.0), 0, 0, 1))
 	
 	image.unlock()
+	heightImage.unlock()
 	
 	self.biomeMap.create_from_image(image)
 	print("biome ", biomeMap.get_data().get_height(), biomeMap.get_data().get_width(), ", ", self.biomeMap.get_data())
 	self.get_surface_material(0).set_shader_param("biome_map", biomeMap)
+	self.get_surface_material(0).set_shader_param("seed", rng.randf_range(0, pow(2.0, 63)))
 	
 	#Paso de refinamiento de terreno usando técnicas de sistemas inteligentes
 	#El texturizado se hace desde el fragment shader dependiendo de clima, humedad, altura, bioma, etc.
--- procedural_generatorMKI/terraformer.shader
+++ procedural_generatorMKI/terraformer.shader
@@ -1,14 +1,42 @@
 shader_type spatial;
 //render_mode unshaded;
 
+uniform float minTemp = 0.0;
 uniform float height_scale = 0.5;
+uniform float seed = 43758.5453;
+uniform float sea_level = 0.0;
 uniform sampler2D map;
 uniform sampler2D biome_map;
 uniform vec4 color;
 
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(seed * 0.000297, seed * 0.00178))) * seed);
+}
+
+float rand_range(vec2 co, float inf, float sup){
+	return (rand(co) * (sup - inf)) + inf;
+}
+
+float lerp(float a, float b, float t){
+	return (1.0 - t) * a + b * t;
+}
+
+float invLerp(float a, float b, float v){
+	return (v - a) / (b - a);
+}
+
+float remap(float iMin, float iMax, float oMin, float oMax, float v){
+	float t = invLerp(iMin, iMax, v);
+	return lerp(oMin, oMax, t);
+}
+
 void vertex() {
-	float height = texture(map, VERTEX.xz / 2.0 + 0.5).x * height_scale;
-	//float height = fbm(VERTEX.xz * 4.0) * height_scale;
+	float tempRanges = 9.0;
+	float height = texture(map, VERTEX.xz / 2.0 + 0.5).x;
+	//if(height >= 8.0 / tempRanges){
+	//	texture(biome_map, VERTEX.xz / 2.0 + 0.5).x = rand_range(VERTEX.xz, remap(0, 30.0, 0, 1.0, minTemp), remap(0, 30.0, 0, 1.0, 2.0 * minTemp / 30.0));
+	//}
+	height *= height_scale;
 	VERTEX.y += height * 0.5;
   	COLOR.xyz = vec3(height) * texture(biome_map, VERTEX.xz / 2.0 + 0.5).xyz;
 	vec2 e = vec2(0.01, 0.0);
